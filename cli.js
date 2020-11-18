c5ede08d0c7ca2b90a2bcefc96fe59806a65efbe
JeremyJaydan <Jeremy@Parmenter.io>
2020-11-18 09:03:26 +0000

    fixed flags to return booleans instead of empty arrays

--- cli.js
+++ cli.js
@@ -8,10 +8,10 @@ if(url){
   const options = {
     force: Deno.args.filter(arg => {
       return arg === "--force"
-    }),
+    }).length > 0,
     forceRecursive: Deno.args.filter(arg => {
       return arg === "--force-recursive";
-    })
+    }).length > 0
   };
   
   const loc = new Location(Deno.cwd());
@@ -31,32 +31,3 @@ if(url){
     
   `);
 }
-
-// import { Command } from "https://deno.land/x/cliffy@v0.15.0/command/mod.ts";
-
-// const cmd = new Command()
-//   .name("mod")
-//   .version("0.0.0")
-//   .description("Multi content type vendering tool")
-  
-//   .command("import <url>")
-//   .description("Import anything* locally or from the wild")
-//   .option("-f, --force [overwrite:boolean]", "overwrite existing module", {
-//     default: false
-//   })
-//   .option("-r, --force-recursive [overwrite:boolean]", "recursively overwrite existing modules", {
-//     default: false
-//   })
-//   .action((options, url) => {
-    
-//     const loc = new Location(Deno.cwd());
-//     const ref = new ResourceReference(url);
-    
-//     loc.import(ref, options)
-//       .then(() => {
-//         console.log("Done!");
-//       });
-      
-//   })
-  
-//   .parse(Deno.args);
